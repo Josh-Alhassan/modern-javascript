4553fb1915ded84b398f89a71d306b1f3323dfc1
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-01-23 06:56:38 +0000

    Default Parameters

--- Closer Look at Functions/script.js
+++ Closer Look at Functions/script.js
@@ -1,1 +1,19 @@
 'use strict' 
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
+    const booking = {
+        flight,
+        numPassengers,
+        price,
+    };
+
+    console.log(booking);
+    bookings.push(booking);
+}
+
+console.log("LH123");
+console.log("LH123", 2, 800)
+console.log("LH123", 5)
+console.log("LH213", undefined, 1000)
